b62d251b733ca0766f5e1c765a1cd15b5b26bfa1
Mark Dingemanse <mark@sandwave.io>
2020-09-15 18:53:04 +0200

    cs fix

---
+++ tests/Domain/data/domain_details_invalid.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types = 1);
+
+return [
+    'domainName' => 'example.nl',
+    'registry' => 'sidn',
+    'customer' => 'johndoe',
+    'registrant' => 'johndoe',
+    'privacyProtect' => false,
+    'status' => 'PENDING_RENEW',
+    'authcode' => '294759302',
+    'languageCode' => 'nl',
+    'autoRenew' => true,
+    'autoRenewPeriod' => 12,
+    'ns' => [
+        'ns01.example.com',
+        'ns02.example.com',
+    ],
+    'childHosts' => [
+        'example.com',
+    ],
+    'createdDate' => '2020-08-30 01:02:03',
+    'updatedDate' => '2020-08-30 01:02:03',
+    'expiryDate' => '2020-11-30 01:02:03',
+    'premium' => false,
+    'zone' => include __DIR__ . '/zone_valid.php',
+    'contacts' => [
+        include __DIR__ . '/domain_contact_valid.php',
+        include __DIR__ . '/domain_contact_valid.php',
+    ],
+    'keyData' => [
+        include __DIR__ . '/key_data_valid.php',
+        include __DIR__ . '/key_data_valid.php',
+    ],
+    'ds_data' => [
+        include __DIR__ . '/ds_data_valid.php',
+        include __DIR__ . '/ds_data_valid.php',
+    ],
+];
---
+++ tests/Domain/data/key_data_invalid.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+
+return [
+    'protocol' => 3,
+    'flags' => 256,
+    'algorithm' => 100001,
+    'publicKey' => '---RSA--- asdfasdfasdfasdfasdf',
+];
