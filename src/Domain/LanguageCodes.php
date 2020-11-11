aeac22b42a72435251c52b1acc0443ecd0961e6a
Thomas Ploegaert <thomas@sandwave.io>
2020-11-11 17:57:38 +0100

    added optional parameters to domainsyntax, added languagecodes

---
+++ src/Domain/LanguageCodes.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace SandwaveIo\RealtimeRegister\Domain;
+
+final class LanguageCodes implements DomainObjectInterface
+{
+    /** @var string */
+    public $name;
+
+    /** @var string|null */
+    public $allowedCharacters;
+
+    private function __construct(string $name, ?string $allowedCharacters)
+    {
+        $this->name = $name;
+        $this->allowedCharacters = $allowedCharacters;
+    }
+
+    public static function fromArray(array $json): LanguageCodes
+    {
+        return new LanguageCodes(
+            $json['name'],
+            $json['allowedCharacters'] ?? null
+        );
+    }
+
+    public function toArray(): array
+    {
+        return array_filter([
+            'name'              => $this->name,
+            'allowedCharacters' => $this->allowedCharacters,
+        ], function ($x) {
+            return ! is_null($x);
+        });
+    }
+}
