33e783ddd0e8739a9da05118ac9d877ea322ccf3
Stephan van der Horn <stephan@sandwave.io>
2021-10-20 22:34:43 +0200

    Fixed domain transfer response, added correct response in test

---
+++ tests/Domain/data/domain_transfer_valid.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+return [
+    'domainName' => 'example.nl',
+    'status' => 'pending',
+    'requestedDate' => '2020-03-04T12:34:56Z',
+    'expiryDate' => '2020-05-04T12:34:56Z',
+    'type' => 'IN',
+    'processId' => 5,
+];
