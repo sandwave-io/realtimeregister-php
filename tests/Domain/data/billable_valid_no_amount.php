6e96b93703d2f1f6231e86e97641b1392a3bf952
Arno Westerhoff <arno@sandwave.io>
2021-11-05 15:47:36 +0100

    Fix tests

--- tests/Domain/data/billable_valid_no_amount.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-return [
-    'product' => '.nl',
-    'action' => 'TRANSFER',
-    'quantity' => 1,
-    'providerName' => 'sidn',
-];
--- tests/Domain/data/notification_poll_valid_only_required.php
+++ tests/Domain/data/notification_poll_valid_only_required.php
@@ -1,5 +1,5 @@
 <?php declare(strict_types = 1);
 
 return [
-    'count' => '1',
+    'count' => 1,
 ];
