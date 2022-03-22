5bb993f9c60aec195209f4582834a245357b7b3c
Thijs Wiersema <thijs@sandwave.io>
2022-03-22 16:21:39 +0100

    added process info request

---
+++ tests/Domain/data/process_info_valid.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+
+return [
+    'commonName' => 'foobar.dev',
+    'requiresAttention' => false,
+    'validations' => [['some' => 'array', 'with' => 'validations info' ]],
+    'notes' => [['some' => 'array', 'with' => 'notes']],
+];
