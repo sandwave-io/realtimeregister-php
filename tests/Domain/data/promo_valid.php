032bca6e27fd6e90e1c1b3f54449eb728610350a
Hylke Witjens <hylke.witjens@realtimeregister.com>
2024-09-26 15:04:24 +0200

    Added promo call to customers api

---
+++ tests/Domain/data/promo_valid.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+return [
+    'product' => 'domain_org',
+    'action' => 'CREATE',
+    'currency' => 'EUR',
+    'price' => 99,
+    "fromDate" => "2024-01-01T00:00:00Z",
+    "endDate" => "2025-01-01T00:00:00Z",
+    "active" => true
+];
