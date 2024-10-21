c505dd676a88ceea71093dfd08797f4a21b24501
Romke van der Meulen <romke@sandwave.io>
2024-10-21 07:54:54 +0000

    Ssl

---
+++ src/Domain/ResendDcvCollection.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace SandwaveIo\RealtimeRegister\Domain;
+
+class ResendDcvCollection extends AbstractCollection
+{
+    /** @var ResendDcv[] */
+    public array $entities;
+
+    public static function fromArray(array $json): ResendDcvCollection
+    {
+        return parent::fromArray($json);
+    }
+
+    public function offsetGet($offset): ?ResendDcv
+    {
+        return $this->entities[$offset] ?? null;
+    }
+
+    public static function parseChild(array $json): ResendDcv
+    {
+        return ResendDcv::fromArray($json);
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'dcv' => parent::toArray(),
+        ];
+    }
+}
